3d9f4d93639a0264bdf78a1944460505c9949be5
havardeidheim <eidheim@live.no>
2026-01-30 15:29:46 +0100

    Update colors

--- source/ts/game-objects/inter-block.ts
+++ source/ts/game-objects/inter-block.ts
@@ -1,11 +1,16 @@
 import { GameObject } from './game-object.js';
-import { COLOR_FILL, COLOR_STROKE } from '../utils/colors.js';
+import { COLOR_FILL, COLOR_INTER_BLOCK } from '../utils/colors.js';
+import { DIMENSION_STATIC } from '../utils/constants.js';
 
 export class InterBlock extends GameObject {
+    constructor(x: number = 0, y: number = 0, width: number = 0, height: number = 0, _dimension: number = 0) {
+        super(x, y, width, height, DIMENSION_STATIC);
+    }
+
     render(ctx: CanvasRenderingContext2D): void {
         ctx.fillStyle = COLOR_FILL;
         ctx.fillRect(this.x, this.y, this.width, this.height);
-        ctx.strokeStyle = COLOR_STROKE;
+        ctx.strokeStyle = COLOR_INTER_BLOCK;
         ctx.lineWidth = 2;
         ctx.strokeRect(this.x, this.y, this.width, this.height);
     }
--- source/ts/utils/colors.ts
+++ source/ts/utils/colors.ts
@@ -1,5 +1,5 @@
 export const COLOR_BACKGROUND = '#708090';
-export const COLOR_FILL = '#ffffff';
+export const COLOR_FILL = '#54606c';
 export const COLOR_STROKE = '#ff00ff';
 
 export const COLOR_DANGER_RED = '#ff0000';
@@ -7,6 +7,7 @@ export const COLOR_FALLING_BLOCK = 'yellow';
 export const COLOR_BLOCK = 'teal';
 export const COLOR_CHECKPOINT = 'green';
 export const COLOR_GOAL = 'orange';
+export const COLOR_INTER_BLOCK = '#000000';
 
 export const COLOR_TEXT_BACKGROUND = '#f5f5dc';
 export const COLOR_TEXT_STROKE = '#000000';
