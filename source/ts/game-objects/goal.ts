af3a8c293f11795f239763cb91f65909c5b4be1a
havardeidheim <eidheim@live.no>
2026-02-01 09:46:23 +0100

    Responsive scaling and goal visuals

--- source/ts/game-objects/goal.ts
+++ source/ts/game-objects/goal.ts
@@ -5,6 +5,8 @@ import type { Player } from './player.js';
 import type { Game } from '../game.js';
 
 export class Goal extends GameObject {
+    reached: boolean = false;
+
     render(ctx: CanvasRenderingContext2D): void {
         const circleDiameter = 40;
         const cx = this.x + this.width / 2;
@@ -13,6 +15,14 @@ export class Goal extends GameObject {
         const halfRectW = r / 2;
         const rectBottom = this.y + this.height - 1.5;
         const intersectY = cy + r * Math.sqrt(3) / 2;
+
+        if (this.reached) {
+            ctx.beginPath();
+            ctx.arc(cx, cy, r + 10, 0, 2 * Math.PI);
+            ctx.strokeStyle = '#FFFFFF';
+            ctx.lineWidth = 3;
+            ctx.stroke();
+        }
 
         ctx.beginPath();
         ctx.moveTo(cx - halfRectW, rectBottom);
@@ -30,6 +40,7 @@ export class Goal extends GameObject {
 
     resolvePlayerCollision(player: Player, normal: Point, game: Game): boolean {
         if (this.getBounds().intersects(player.getBounds())) {
+            this.reached = true;
             game.win();
         }
         return false;
